00b3674a4cca4af55219a104ff9798e2da4749de
Ankar Kumar Saha <ankarkumarankar572@gmial.com>
2024-01-19 14:08:37 +0600

    cpm updated and all the forecasting time series is update to right

--- CPM.py
+++ CPM.py
@@ -1,91 +1,102 @@
 import numpy as np
 
-class Activity:
-    
-    def __init__(self, idx, activity, duration) -> None:
-        self.idx = idx
-        self.activity = activity
-        self.duration = duration
-        self.predecessor = []
-        self.successor = []
-        self.early_start = 0
-        self.early_finish = 0
-        self.late_start = 0
-        self.late_finish = 0
+from collections import defaultdict 
 
-file_path = "input2.txt"
-
-activities = {}
+file_path = "cpm.txt"
+ 
+activities =defaultdict(list) 
+activities2 =defaultdict(list) 
 index = []
-
+MX = 1005
+arr = np.zeros((MX, 2), dtype=int)  # arr[mx][2] initialize all with 0
+cost = np.zeros((MX, 5), dtype=int)
+i=1
 for lines in open(file_path):
      words = lines.rstrip('\n').split(',')
     #  print(words)
-     idx = int(words[0])
-     activity = words[1]
-     duration = int(words[2])
+     idx= int(words[0]) ## when we declare an extra index value then needed 
+     arr[i][1] = words[1] # activity name 
+     cost[i][0] = int(words[2])
      predecessors = words[3]
+   #   print(i,predecessors)
+     index.append(idx)
 
-     index.append(idx) #  index list store idx or activity
-
-
-     activities[idx] = Activity(idx, activity, duration)
+   
 #  1 key ---> 1,A,8 erkm value thakbe
 
-     if (predecessors != ""):
-        predecessors = predecessors.split(';')
+     if(len(predecessors)!=0):
+        predecessors = predecessors.split(';')  # how you take input 
+      #   print(i,predecessors)
         for item in predecessors:
             item = int(item)
+            # print(i,item)
             # print (str(idx) + " predecessor: " + str(item))
-            activities[idx].predecessor.append(item)
-            activities[item].successor.append(idx)
+            activities[idx].append(item)  # for forword pass needed
+            activities2[item].append(idx)  # for backward pass needed
+     i=i+1
 
+
+### check the value of graph if needed 
 # for idx in index:
-#     print (idx, activities[idx].predecessor)
-#     print (idx, activities[idx].successor)
+#    print(idx,'>')
+#    for val in activities2[idx]:
+#        print(val,end='   ')
+#    #  print (idx, activities[idx])
+     
+
+
 
 maxEF = 0
 
 for idx in index:
-    if (len(activities[idx].predecessor) == 0):
-        activities[idx].early_finish = activities[idx].duration
+    if (len(activities[idx]) == 0):
+        cost[idx][2]=cost[idx][0]  ## earliest finish a duration time store krbo
     else :
         maxTime = 0
-        for item in activities[idx].predecessor:# sob gula activity max EF time check korbo
-            
-                maxTime = max(maxTime,activities[item].early_finish)
+        for item in activities[idx]:# sob gula activity max EF time check korbo            
+                maxTime = max(maxTime,cost[item][2])
+        cost[idx][1]=maxTime
+        cost[idx][2]=maxTime+cost[idx][0]
 
-        activities[idx].early_start = maxTime # max EF time ta add kore dibo next ES time te
-        activities[idx].early_finish = maxTime + activities[idx].duration
-    
-    # print(activities[idx].early_start, activities[idx].early_finish)
-    maxEF = max(maxEF, activities[idx].early_finish) # finish line e gia abar MAX EF time check kore store korbo
+     
+    maxEF = max(maxEF, cost[idx][2]) # finish line e gia abar MAX EF time check kore store korbo
+
 
 
 length = len(index)
 for i in range(length):
     idx = index[length-i-1]
-    if (len(activities[idx].successor) == 0):
-        activities[idx].late_finish = maxEF
-        activities[idx].late_start = maxEF - activities[idx].duration
+    if (len(activities2[idx]) == 0):  ## when there is no successor 
+        cost[idx][4]=maxEF   ## latest finish a max value store krbo
+        cost[idx][3]=maxEF-cost[idx][0]
     else :
         minTime = 99999
-        for item in activities[idx].successor:
-            if (minTime > activities[item].late_start):
-                minTime =min ( minTime, activities[item].late_start)
-            
-        activities[idx].late_finish = minTime
-        activities[idx].late_start = minTime - activities[idx].duration
-    
-    # print(activities[idx].late_start, activities[idx].late_finish)
+        for item in activities2[idx]:           
+                minTime =min( minTime, cost[item][3])
+        cost[idx][4]=minTime
+        cost[idx][3]=minTime-cost[idx][0]   
+      
 
 result = []
+print()
+print("check the value of cost array")
+for i in index:
+     print()
+    
+     # earliest_start -> earliest_finish -> latest_start -> latest_finish
+     for j in range(1,5):
+      print(cost[i][j],end='   ')#,cost[i][j] ,end='   ',cost[i][3] ,end='   ', cost[i][4])
+     if cost[i][1] == cost[i][3] and cost[i][2] == cost[i][4]:
+      #   result.append(arr[i][1]) # when we have not extra column for activity name
+         result.append(i)    ## when index and activity order are same
 
-for idx in index:
-    if (activities[idx].early_finish == activities[idx].late_finish):
-        result.append(activities[idx].activity)
+print()
+print()
 
+print("critical path ")
 for i in range(len(result)-1):    
-    print(result[i] + " -> ", end="")
+    print(result[i] , " -> ", end="")
 
 print(result[len(result)-1])
+
+
--- PERT.py
+++ PERT.py
@@ -41,3 +41,4 @@ for i in range(1, n + 1):
 		print(i, end=" > ")
 
 
+
